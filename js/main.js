c18c237a670a16f1a07a0542dec85191a2c1859e
Imam <imam.tauhid.dar@icloud.com>
2018-02-27 01:09:51 +0700

    up 1

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 * @Author: Imam
 * @Date:   2018-02-25 03:18:52
 * @Last Modified by:   Imam
-* @Last Modified time: 2018-02-26 03:23:49
+* @Last Modified time: 2018-02-27 01:00:13
 */
 $(document).ready(function () {
 	var $formsub = $('form#subemail')
+	var $formsubmbl = $('form#subemail-mbl')
 
 	// var DOMAIN = 'http://localhost:8080/v2'
 	var DOMAIN = 'http://api.rumaji.com/v2' 
@@ -26,6 +27,20 @@ $(document).ready(function () {
 		return fetch(url, options)
 			.then(resp => resp.json())
 	}
+
+	$formsubmbl.submit(function(e) {
+		e.preventDefault()
+		var self = this
+		var $email = $('#emailmobile')
+		var email = $email.val()
+		// check if email already exist 
+		if(!email) return alert('insert your mail first')
+		requestLayer('POST', '/user/subscribe', {email:email})
+		$(self).hide()
+		$('.splash-subhead').append('<p class="done-reg-mbl">Terima Kasih, alamat email "'+email+'" telah kami registrasikan.</p>')
+		// subs email 
+		// redirect to success
+	}) 
 	
 	$formsub.submit(function(e) {
 		e.preventDefault()
